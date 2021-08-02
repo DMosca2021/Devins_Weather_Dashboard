6eba63e303bb086bacaf776787c81ef2885a997e
dMosca <devinmosca@yahoo.com>
2021-08-01 18:15:17 -0600

    Starting JS functions

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,20 @@
-console.log("Welcome to the weather")
+// console.log("Welcome to the weather")
 
 
 const APIkey = "76cc07c4bbc6bedc0eceb1791bd17796";
-let inputEl = document.querySelector("#city-input");
-let searchBtn = document.querySelector("#button-addon2");
-let histEL = document.querySelector("#search-history");
-let clearHist = document.querySelector("#clear-history");
-let cityName = document.querySelector("#city-name");
-let cityTemp = document.querySelector("#temp");
-let cityWind = document.querySelector("#wind-speed");
-let cityHumid = document.querySelector("#humidity");
-let cityUv = document.querySelector("#uvIndex");
+// let inputEl = document.querySelector("#city-input");
+// let searchBtn = document.querySelector("#button-addon2");
+// let histEL = document.querySelector("#search-history");
+// let clearHist = document.querySelector("#clear-history");
+// let currentCity = document.querySelector("#city-name");
+// let cityTemp = document.querySelector("#temp");
+// let cityWind = document.querySelector("#wind-speed");
+// let cityHumid = document.querySelector("#humidity");
+// let cityUv = document.querySelector("#uvIndex");
+
+function searchWeather(cityName) {
+    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIkey;
+    fetch(queryURL)
+        .then(function(response){
+            console.log(response);
+        })
